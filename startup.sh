1192ffb841136199b8338f1fbdd8c940d5f0ebe3
Franz Schauer <fschauer@outook.de>
2023-12-05 21:18:53 +0100

    [FIX] Volume mixer workaround

--- startup.sh
+++ startup.sh
@@ -4,17 +4,19 @@
 LOG_FILE="/data/startup.log"
 echo "startup ..." > ${LOG_FILE}
 
-# Function to set the startup volume in the corresponding config file:
-# $1: Startup volume level
-function set_startup_volume {
-    VOLUME=$1
+# Function to set the startup options in the corresponding config file:
+function set_startup_options {
+    VOLUME=disabled
+    MIXER=PCM
 
     local CONFIG_FILE="/data/configuration/audio_interface/alsa_controller/config.json"
     local CONFIG_TMP_FILE="/data/configuration/audio_interface/alsa_controller/config.json.tmp"
 
     echo "set startup volume -> ${VOLUME}" >> ${LOG_FILE}
-
     jq ".volumestart.value = \"${VOLUME}\"" ${CONFIG_FILE} > ${CONFIG_TMP_FILE} && mv ${CONFIG_TMP_FILE} ${CONFIG_FILE}
+
+    echo "set mixer control name -> ${MIXER}" >> ${LOG_FILE}
+    jq ".mixer.value = \"${MIXER}\"" ${CONFIG_FILE} > ${CONFIG_TMP_FILE} && mv ${CONFIG_TMP_FILE} ${CONFIG_FILE}
 }
 
 # Function to select sender in queue, set the volume and start playback via the volumio command:
@@ -49,24 +51,21 @@ function start_playback {
     volumio play
 }
 
-# Set startup volume in configuration
-set_startup_volume 60
+# Set startup options in configuration
+set_startup_options
 # Overwrite queue
 cp /data/queue_fm4_b2 /data/queue
 
 # Let volumio start up
-sleep 60
+sleep 20
 
 # Get current date and time
-TIME_API_URL="http://worldtimeapi.org/api/ip.txt"
-UNIXTIME=$(curl -silent ${TIME_API_URL} | grep "^unixtime" | cut -d " " -f 2)
+echo "date: $(date)" >> ${LOG_FILE}
 
-date -d @${UNIXTIME} >> ${LOG_FILE}
-
-day=$(date -d @${UNIXTIME} +%u) # day of week (1..7); 1 is monday
+day=$(date +%u) # day of week (1..7); 1 is monday
 echo day:  $day >> ${LOG_FILE}
 
-time=$(date -d @${UNIXTIME} +%H:%M) # hour (00..23), minute (00..59)
+time=$(date +%H:%M) # hour (00..23), minute (00..59)
 echo time: $time >> ${LOG_FILE}
 
 # Set sender and volume based on the current day and time
@@ -78,7 +77,7 @@ then
     echo "weekends -> ${SENDER}, ${VOLUME}" >> ${LOG_FILE}
 
 else
-    if [[ "$time" < "08:30" ]]
+    if [[ "$time" > "06:00" && "$time" < "08:30" ]]
     then
 
         SENDER="B2"
